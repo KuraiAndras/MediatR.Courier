f5f4cf41f9cc9cf093155f953f591a79077e0ca4
Kurai András <k.andras9642@gmail.com>
2020-01-24 21:21:19 +0100

    add injector

---
+++ MediatR.Courier.DependencyInjection/CourierInjector.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatR.Courier.DependencyInjection
+{
+    public static class CourierInjector
+    {
+        public static IServiceCollection AddCourier(this IServiceCollection services)
+        {
+            services.AddSingleton(typeof(INotificationHandler<>), typeof(MediatRCourier<>));
+
+            return services;
+        }
+    }
+}
