5b0de72bac7515c2c4a540515b29464195158115
Kurai András <k.andras9642@gmail.com>
2020-01-25 14:17:53 +0100

    test async action invocation

--- MediatR.Courier.Tests/CourierTests.cs
+++ MediatR.Courier.Tests/CourierTests.cs
@@ -1,4 +1,7 @@
 using MediatR.Courier.TestResources;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -7,6 +10,49 @@ namespace MediatR.Courier.Tests
 {
     public class CourierTests
     {
+        private sealed class AsyncTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new TimeSpan(0L) };
+                yield return new object[] { new TimeSpan(0, 0, 0, 0, 1) };
+                yield return new object[] { new TimeSpan(0, 0, 0, 0, 10) };
+                yield return new object[] { new TimeSpan(0, 0, 0, 0, 100) };
+                yield return new object[] { new TimeSpan(0, 0, 0, 0, 1000) };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        [Theory]
+        [ClassData(typeof(AsyncTestData))]
+        public async Task AsyncActionMightCompleteInTime(TimeSpan delayTime)
+        {
+            var mediatRCourier = new MediatRCourier();
+
+            var receivedMessage = false;
+
+            async void NotificationAction(TestNotification _, CancellationToken __)
+            {
+                await Task.Delay(delayTime).ConfigureAwait(false);
+
+                receivedMessage = true;
+            }
+
+            mediatRCourier.Subscribe<TestNotification>(NotificationAction);
+
+            await mediatRCourier.Handle(new TestNotification(), CancellationToken.None).ConfigureAwait(false);
+
+            if (delayTime.Ticks == 0)
+            {
+                Assert.True(receivedMessage);
+            }
+            else
+            {
+                Assert.False(receivedMessage);
+            }
+        }
+
         [Fact]
         public async Task SubscribedActionInvoked()
         {
