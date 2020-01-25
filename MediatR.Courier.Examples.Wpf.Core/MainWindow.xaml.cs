a6ffcc191d7ddd7e6c7e3e3401f2b435acea9651
Kurai András <k.andras9642@gmail.com>
2020-01-25 15:37:32 +0100

    add wpf example project

---
+++ MediatR.Courier.Examples.Wpf.Core/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace MediatR.Courier.Examples.Wpf.Core
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
