717c8d8e6122d68bb88a9f017f62a0d56d8a47c7
Kurai András <k.andras9642@gmail.com>
2020-01-25 15:54:28 +0100

    Composition root

---
+++ MediatR.Courier.Examples.Wpf.Core/DependencyInjection/CompositionRoot.cs
@@ -0,0 +1,16 @@
+﻿using MediatR.Courier.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MediatR.Courier.Examples.Wpf.Core.DependencyInjection
+{
+    public static class CompositionRoot
+    {
+        private static readonly IServiceProvider _serviceProvider = new ServiceCollection()
+            .AddMediatR(AppDomain.CurrentDomain.GetAssemblies())
+            .AddCourier(AppDomain.CurrentDomain.GetAssemblies())
+            .BuildServiceProvider();
+
+        public static T Resolve<T>() => _serviceProvider.GetService<T>();
+    }
+}
