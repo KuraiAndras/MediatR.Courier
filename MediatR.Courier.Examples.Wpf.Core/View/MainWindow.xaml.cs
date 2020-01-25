bfa68b989bc78dd2456cf8ddf01921b48fe9d65c
Kurai András <k.andras9642@gmail.com>
2020-01-25 17:05:51 +0100

    Open window button

--- MediatR.Courier.Examples.Wpf.Core/View/MainWindow.xaml.cs
+++ MediatR.Courier.Examples.Wpf.Core/View/MainWindow.xaml.cs
@@ -5,5 +5,15 @@ namespace MediatR.Courier.Examples.Wpf.Core.View
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void OpenWindowClicked(object sender, RoutedEventArgs e)
+        {
+            var window = new Window
+            {
+                Content = new MainControl()
+            };
+
+            window.Show();
+        }
     }
 }
