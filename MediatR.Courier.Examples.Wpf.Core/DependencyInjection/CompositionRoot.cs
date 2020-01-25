9a030362cf277cf4e1a844c322a7d4b844f19efc
Kurai András <k.andras9642@gmail.com>
2020-01-25 17:05:33 +0100

    cleanup

--- MediatR.Courier.Examples.Wpf.Core/DependencyInjection/CompositionRoot.cs
+++ MediatR.Courier.Examples.Wpf.Core/DependencyInjection/CompositionRoot.cs
@@ -1,24 +1,18 @@
 ﻿using MediatR.Courier.DependencyInjection;
+using MediatR.Courier.Examples.Shared.Notifications;
 using MediatR.Courier.Examples.Wpf.Core.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using MediatR.Courier.Examples.Shared.Notifications;
 
 namespace MediatR.Courier.Examples.Wpf.Core.DependencyInjection
 {
     public static class CompositionRoot
     {
-        private static readonly IServiceProvider _serviceProvider;
-
-        static CompositionRoot()
-        {
-            var services = new ServiceCollection()
-                .AddMediatR(AppDomain.CurrentDomain.GetAssemblies())
-                .AddCourier(typeof(ExampleNotification).Assembly)
-                .AddTransient<IExampleViewModel, ExampleViewModel>();
-            _serviceProvider = services
-                .BuildServiceProvider();
-        }
+        private static readonly IServiceProvider _serviceProvider = new ServiceCollection()
+            .AddMediatR(AppDomain.CurrentDomain.GetAssemblies())
+            .AddCourier(typeof(ExampleNotification).Assembly)
+            .AddTransient<IExampleViewModel, ExampleViewModel>()
+            .BuildServiceProvider();
 
         public static T Resolve<T>() => _serviceProvider.GetService<T>();
     }
