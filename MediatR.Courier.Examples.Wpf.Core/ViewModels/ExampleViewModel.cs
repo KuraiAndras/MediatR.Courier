00a796af8c6ccb4d6ee2a525c6c24b00e7f0292b
Kurai András <k.andras9642@gmail.com>
2020-01-25 16:35:13 +0100

    implement view model

--- MediatR.Courier.Examples.Wpf.Core/ViewModels/ExampleViewModel.cs
+++ MediatR.Courier.Examples.Wpf.Core/ViewModels/ExampleViewModel.cs
@@ -1,12 +1,42 @@
-﻿namespace MediatR.Courier.Examples.Wpf.Core.ViewModels
+﻿using MediatR.Courier.Examples.Shared.Notifications;
+using MediatR.Courier.Examples.Shared.Requests;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR.Courier.Examples.Wpf.Core.ViewModels
 {
     public sealed class ExampleViewModel : ViewModelBase, IExampleViewModel
     {
+        private int _notificationCount;
+
         public ExampleViewModel(
             IMediator mediator,
             ICourier courier)
-            : base(mediator, courier)
+            : base(mediator, courier) =>
+            Courier.Subscribe<ExampleNotification>(ExampleNotificationFired);
+
+        private void ExampleNotificationFired(ExampleNotification notification, CancellationToken _) => NotificationCount = notification.NotificationCount;
+
+        public async Task Initialize() => NotificationCount = await Mediator.Send(new NotificationCountQuery()).ConfigureAwait(false);
+        public async Task IncrementNotificationCount() => await Mediator.Send(new IncrementCallCountCommand()).ConfigureAwait(false);
+
+        public int NotificationCount
         {
+            get => _notificationCount;
+            private set => SetAndNotifyPropertyChanged(ref _notificationCount, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetAndNotifyPropertyChanged<T>(ref T backingField, T value, [CallerMemberName] string propertyName = default)
+        {
+            if (backingField.Equals(value)) return;
+
+            backingField = value;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- MediatR.Courier.Examples.Wpf.Core/ViewModels/IExampleViewModel.cs
+++ MediatR.Courier.Examples.Wpf.Core/ViewModels/IExampleViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace MediatR.Courier.Examples.Wpf.Core.ViewModels
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace MediatR.Courier.Examples.Wpf.Core.ViewModels
 {
-    public interface IExampleViewModel
+    public interface IExampleViewModel : INotifyPropertyChanged
     {
+        Task Initialize();
+        Task IncrementNotificationCount();
+        int NotificationCount { get; }
     }
 }
