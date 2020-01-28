377fc32153984740411121898b5a75f4e1a3cf07
Kurai András <k.andras9642@gmail.com>
2020-01-28 17:37:20 +0100

    courier client

---
+++ MediatR.Courier/ICourierNotificationHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR.Courier
+{
+    public interface ICourierNotificationHandler<in TNotification> where TNotification : INotification
+    {
+        Task Handle(TNotification notification, CancellationToken cancellationToken = default);
+    }
+}
