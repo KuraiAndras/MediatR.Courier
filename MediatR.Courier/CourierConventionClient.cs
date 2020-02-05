0c8ecfb88cd3aca91f68c6884a67f43baeeab8b3
Kurai András <k.andras9642@gmail.com>
2020-02-05 19:38:54 +0100

    Courier convention client

---
+++ MediatR.Courier/CourierConventionClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediatR.Courier
+{
+    public abstract class CourierConventionClient : IDisposable
+    {
+        private readonly ICourier _courier;
+        private readonly IReadOnlyCollection<object> _actions;
+
+        protected CourierConventionClient(ICourier courier)
+        {
+            _courier = courier;
+
+            // TODO: Register actions on courier
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            foreach (var @delegate in _actions)
+            {
+                // TODO: unsubscribe
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
