80f60587bee81a9c0e50d76c5e030289acd087f3
Kurai András <k.andras9642@gmail.com>
2020-02-07 18:33:09 +0100

    add fiorst convention test

---
+++ MediatR.Courier.TestResources/TestConventionClient.cs
@@ -0,0 +1,13 @@
+﻿namespace MediatR.Courier.TestResources
+{
+    public sealed class TestConventionClient : CourierConventionClient
+    {
+        public TestConventionClient(ICourier courier) : base(courier)
+        {
+        }
+
+        public bool MessageReceived { get; private set; }
+
+        public void Handle(TestNotification notification) => MessageReceived = true;
+    }
+}
---
+++ MediatR.Courier.Tests/ConventionClientTests.cs
@@ -0,0 +1,22 @@
+﻿using MediatR.Courier.TestResources;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MediatR.Courier.Tests
+{
+    public sealed class ConventionClientTests
+    {
+        [Fact]
+        public async Task ConventionClientMethodInvoked()
+        {
+            var courier = new MediatRCourier();
+
+            var testClient = new TestConventionClient(courier);
+
+            await courier.Handle(new TestNotification(), CancellationToken.None).ConfigureAwait(false);
+
+            Assert.True(testClient.MessageReceived);
+        }
+    }
+}
