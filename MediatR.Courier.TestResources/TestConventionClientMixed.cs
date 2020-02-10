3e8e2e3aec190eea1fb84664003a10bfa759399f
Kurai András <k.andras9642@gmail.com>
2020-02-10 19:42:07 +0100

    Check return parameter in convention client

---
+++ MediatR.Courier.TestResources/TestConventionClientMixed.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace MediatR.Courier.TestResources
+{
+    public sealed class TestConventionClientMixed : CourierConventionClient, ICarryNotifications
+    {
+        public TestConventionClientMixed(ICourier courier) : base(courier)
+        {
+        }
+
+        public int MessageReceivedCount { get; private set; }
+        public int ProperlyImplementedHandleCount => 2;
+
+        public void Handle(TestNotification _) => MessageReceivedCount++;
+        public void Handle(TestNotification _, CancellationToken __) => MessageReceivedCount++;
+        public void Handle(TestNotification _, CancellationToken __, TestConventionClient1Cancellation ___) => MessageReceivedCount++;
+        public int HandleReturnsInt(TestNotification _) => MessageReceivedCount++;
+    }
+}
--- MediatR.Courier.TestResources/TestInterfaceClient1Cancellation.cs
+++ MediatR.Courier.TestResources/TestInterfaceClient1Cancellation.cs
@@ -19,18 +19,4 @@ namespace MediatR.Courier.TestResources
         public int MessageReceivedCount { get; private set; }
         public int ProperlyImplementedHandleCount => 1;
     }
-
-    public sealed class TestConventionClient3Mixed : CourierConventionClient, ICarryNotifications
-    {
-        public TestConventionClient3Mixed(ICourier courier) : base(courier)
-        {
-        }
-
-        public int MessageReceivedCount { get; private set; }
-        public int ProperlyImplementedHandleCount => 2;
-
-        public void Handle(TestNotification _) => MessageReceivedCount++;
-        public void Handle(TestNotification _, CancellationToken __) => MessageReceivedCount++;
-        public void Handle(TestNotification _, CancellationToken __, TestConventionClient1Cancellation ___) => MessageReceivedCount++;
-    }
 }
