46aef956cc9921f6e3a7c147aeddc26a90ae21b5
Kurai András <k.andras9642@gmail.com>
2022-06-17 00:29:18 +0200

    add clean generic tests project

---
+++ MediatR.Courier.GenericTests/GenericTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatR.Courier.GenericTests;
+
+public class GenericTest
+{
+    public record GenericMessage<T>(T Data) : INotification;
+
+
+    public class GenericHandler<T> : INotificationHandler<GenericMessage<T>>
+    {
+        public Task Handle(GenericMessage<T> notification, CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+
+    [Fact]
+    public void This_Should_Not_Throw()
+    {
+        var services = new ServiceCollection()
+            .AddMediatR(typeof(GenericTest).Assembly);
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        mediator.Publish(new GenericMessage<string>("Hello"));
+    }
+
+    [Fact]
+    public void This_Should_Not_Throw_Also()
+    {
+        var services = new ServiceCollection()
+            .AddMediatR(typeof(GenericTest).Assembly)
+            .AddTransient(typeof(INotificationHandler<>), typeof(GenericHandler<>));
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+        mediator.Publish(new GenericMessage<string>("Hello"));
+    }
+}
