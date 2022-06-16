3369bb5e7dab730ccdb72f9f561c5a21273ceb23
Kurai András <k.andras9642@gmail.com>
2022-06-16 22:52:36 +0200

    add non-failing test sadly

---
+++ MediatR.Courier.Tests/GenericTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MediatR.Courier.Tests;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Tests")]
+public class GenericTests
+{
+    public record Message<T>(T Data) : INotification;
+
+    [Fact]
+    public async Task Generic_Message_Is_Handled()
+    {
+        var courier = new MediatRCourier(new() { CaptureThreadContext = false });
+
+        string? data = null;
+
+        void Handler(Message<string> message) => data = message.Data;
+
+        courier.Subscribe<Message<string>>(Handler);
+
+        const string testData = "Hello";
+
+        await courier.Handle(new Message<string>(testData), CancellationToken.None);
+
+        courier.UnSubscribe<Message<string>>(Handler);
+
+        data.Should().Be(testData);
+    }
+}
