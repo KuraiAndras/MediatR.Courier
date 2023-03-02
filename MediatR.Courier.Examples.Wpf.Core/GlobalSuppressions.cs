046f1f28d61a297dbc7062af2b04420378445322
Kurai András <k.andras9642@gmail.com>
2023-03-02 23:57:05 +0100

    remove global supressions

--- MediatR.Courier.Examples.Wpf.Core/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Test scope", Scope = "member", Target = "~M:MediatR.Courier.Examples.Wpf.Core.ViewModels.ExampleViewModel.ExampleNotificationFired(MediatR.Courier.Examples.Shared.Notifications.ExampleNotification,System.Threading.CancellationToken)")]
