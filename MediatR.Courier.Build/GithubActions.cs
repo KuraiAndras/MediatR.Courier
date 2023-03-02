afdb44cc3e391ca4bc189be6f33996a612921d4f
Kurai András <k.andras9642@gmail.com>
2023-03-02 19:45:24 +0100

    update nuke and tools

--- MediatR.Courier.Build/GithubActions.cs
+++ MediatR.Courier.Build/GithubActions.cs
@@ -6,8 +6,25 @@
     GitHubActionsImage.WindowsLatest,
     OnPushTags = new[] { "*" },
     InvokedTargets = new[] { nameof(PushToNuGet) },
-    ImportSecrets = new[] { nameof(NugetApiKey), "GITHUB_TOKEN" },
-    EnableGitHubContext = true
+    ImportSecrets = new[] { nameof(NugetApiKey) },
+    EnableGitHubToken = true
+)]
+[GitHubActions
+(
+    "run-ci",
+    GitHubActionsImage.WindowsLatest,
+    OnPullRequestBranches = new[] { "master", "develop" },
+    OnPushBranches = new[] { "master", "develop" },
+    InvokedTargets = new[] { nameof(RunCi) },
+    ImportSecrets = new[]
+    {
+        nameof(SonarHostUrl),
+        nameof(SonarProjectKey),
+        nameof(SonarToken),
+        nameof(SonarOrganization),
+    },
+    EnableGitHubToken = true,
+    FetchDepth = 0
 )]
 public partial class Build
 {
