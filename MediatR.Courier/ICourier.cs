a87afa27211d22013c0b8eedef6646a874273206
Kurai András <k.andras9642@gmail.com>
2025-06-15 23:51:09 +0200

    use inheritdoc

--- MediatR.Courier/ICourier.cs
+++ MediatR.Courier/ICourier.cs
@@ -15,28 +15,19 @@ public interface ICourier
     void Subscribe<TNotification>(Action<TNotification> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Subscribes a handler to receive notifications of the specified type.
-    /// The handler will be held with a strong reference.
-    /// </summary>
+    /// <inheritdoc cref="Subscribe{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to subscribe to.</typeparam>
     /// <param name="handler">The handler to invoke when a notification is published.</param>
     void Subscribe<TNotification>(Action<TNotification, CancellationToken> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Subscribes a handler to receive notifications of the specified type.
-    /// The handler will be held with a strong reference.
-    /// </summary>
+    /// <inheritdoc cref="Subscribe{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to subscribe to.</typeparam>
     /// <param name="handler">The handler to invoke when a notification is published.</param>
     void Subscribe<TNotification>(Func<TNotification, Task> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Subscribes a handler to receive notifications of the specified type.
-    /// The handler will be held with a strong reference.
-    /// </summary>
+    /// <inheritdoc cref="Subscribe{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to subscribe to.</typeparam>
     /// <param name="handler">The handler to invoke when a notification is published.</param>
     void Subscribe<TNotification>(Func<TNotification, CancellationToken, Task> handler)
@@ -51,28 +42,19 @@ public interface ICourier
     void SubscribeWeak<TNotification>(Action<TNotification> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Subscribes a handler to receive notifications of the specified type using a weak reference.
-    /// The handler will be held with a weak reference, allowing the target object to be garbage collected.
-    /// </summary>
+    /// <inheritdoc cref="SubscribeWeak{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to subscribe to.</typeparam>
     /// <param name="handler">The handler to invoke when a notification is published.</param>
     void SubscribeWeak<TNotification>(Action<TNotification, CancellationToken> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Subscribes a handler to receive notifications of the specified type using a weak reference.
-    /// The handler will be held with a weak reference, allowing the target object to be garbage collected.
-    /// </summary>
+    /// <inheritdoc cref="SubscribeWeak{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to subscribe to.</typeparam>
     /// <param name="handler">The handler to invoke when a notification is published.</param>
     void SubscribeWeak<TNotification>(Func<TNotification, Task> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Subscribes a handler to receive notifications of the specified type using a weak reference.
-    /// The handler will be held with a weak reference, allowing the target object to be garbage collected.
-    /// </summary>
+    /// <inheritdoc cref="SubscribeWeak{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to subscribe to.</typeparam>
     /// <param name="handler">The handler to invoke when a notification is published.</param>
     void SubscribeWeak<TNotification>(Func<TNotification, CancellationToken, Task> handler)
@@ -86,25 +68,19 @@ public interface ICourier
     void UnSubscribe<TNotification>(Action<TNotification> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Unsubscribes a handler from receiving notifications of the specified type.
-    /// </summary>
+    /// <inheritdoc cref="UnSubscribe{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to unsubscribe from.</typeparam>
     /// <param name="handler">The handler to remove from the subscription list.</param>
     void UnSubscribe<TNotification>(Action<TNotification, CancellationToken> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Unsubscribes a handler from receiving notifications of the specified type.
-    /// </summary>
+    /// <inheritdoc cref="UnSubscribe{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to unsubscribe from.</typeparam>
     /// <param name="handler">The handler to remove from the subscription list.</param>
     void UnSubscribe<TNotification>(Func<TNotification, Task> handler)
         where TNotification : INotification;
 
-    /// <summary>
-    /// Unsubscribes a handler from receiving notifications of the specified type.
-    /// </summary>
+    /// <inheritdoc cref="UnSubscribe{TNotification}(Action{TNotification})"/>
     /// <typeparam name="TNotification">The type of notification to unsubscribe from.</typeparam>
     /// <param name="handler">The handler to remove from the subscription list.</param>
     void UnSubscribe<TNotification>(Func<TNotification, CancellationToken, Task> handler)
