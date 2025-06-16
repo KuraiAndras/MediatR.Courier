0c8d06e8aa95cdef7cb3636a3e283ce99e95041c
fretje <github@fretje.be>
2025-06-16 09:46:21 +0200

    add documentation

--- MediatR.Courier/CourierOptions.cs
+++ MediatR.Courier/CourierOptions.cs
@@ -11,5 +11,12 @@ public sealed class CourierOptions
     /// Defaults to false.
     /// </summary>
     public bool CaptureThreadContext { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to use Task.WhenAll to await handler tasks concurrently.
+    /// When set to true, all notification handlers are collected and awaited using Task.WhenAll.
+    /// When set to false, notification handlers are awaited sequentially.
+    /// Defaults to false.
+    /// </summary>
     public bool UseTaskWhenAll { get; set; }
 }
